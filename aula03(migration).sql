1a6df245a808faa47ee6bb40e7d44db7133e1e5b
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-10-14 21:13:21 -0300

    JOIN com carro e tipo_carro

--- aula03(migration).sql
+++ aula03(migration).sql
@@ -36,22 +36,45 @@ select * from carro
 -- DROP TABLE carro;
 -- DROP TABLE tipo_carro;
 
-INSERT INTO carro (id_tipo, marca, modelo, ano, cor, chassi)
+INSERT INTO carro (id_tipo_carro, marca, modelo, ano, cor, chassi)
 VALUES
--- SUVs (id_tipo = 1)
+-- SUVs (id_tipo_carro = 1)
 (1, 'Toyota', 'RAV4', 2021, 'Preto', '9BWZZZ377VT004251'),
 (1, 'Honda', 'CR-V', 2022, 'Branco', '8ABZZZ123YT992311'),
 
--- Hatch (id_tipo = 2)
+-- Hatch (id_tipo_carro = 2)
 (2, 'Volkswagen', 'Golf', 2020, 'Prata', '1HGCM82633A123456'),
 (2, 'Ford', 'Fiesta', 2019, 'Azul', '3N1BC13E88L999999'),
 
--- Sedan (id_tipo = 3)
+-- Sedan (id_tipo_carro = 3)
 (3, 'Chevrolet', 'Onix Plus', 2023, 'Cinza', '2C3KA63H76H789012'),
 (3, 'Hyundai', 'Elantra', 2021, 'Vermelho', '1HGFA16526L007890'),
 
--- Pickup (id_tipo = 4)
+-- Pickup (id_tipo_carro = 4)
 (4, 'Ford', 'Ranger', 2020, 'Branco', '1FTRW12W47KB34567'),
 
 -- Tipo inexistente (para treinar LEFT JOIN)
 (99, 'Marca Fantasma', 'Modelo X', 2025, 'Transparente', 'XXXXXXXXXXXXXXX');
+
+
+-- Para saber qual o id_tipo_carro carro será, no exemplo acima 1 (SUV)
+select * from tipo_carro 
+
+--conferir o insert
+select * from carro
+
+------------- JOIN -------------
+
+--JOIN sem alias (apelido) 
+SELECT 
+	* 
+FROM
+	carro 
+JOIN tipo_carro  ON carro.id_tipo_carro = tipo_carro.id
+
+--JOIN com alias (apelido) 
+SELECT 
+	c.modelo, tc.tipo, c.chassi
+FROM
+	carro c
+JOIN tipo_carro tc ON c.id_tipo_carro = tc.id
